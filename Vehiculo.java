1fa7f67b0ecf1bfc36bfab9fe445ab8773940416
juanJguerreroG <juanjoguerreroxd@gmail.com>
2026-08-19 07:39:16 -0500

    Add Vehiculo class with vehicle management methods

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,3 +1,6 @@
+/**
+ * Representa un vehículo que ingresa al parqueadero.
+ */
 public class Vehiculo {
         
         private String placa;
@@ -5,6 +8,14 @@ public class Vehiculo {
         private int horaIngreso;
         private Tiquete tiquete;
 
+        /**
+     * Constructor de la clase Vehiculo.
+     *
+     * @param placa placa del vehículo
+     * @param marca marca del vehículo
+     * @param horaIngreso hora de ingreso entre 0 y 23
+     * @param tiquete tiquete asignado al vehículo
+     */
     public Vehiculo(String placa, String marca, int horaIngreso, Tiquete tiquete) {
 
             this.placa = placa;
@@ -13,38 +24,85 @@ public class Vehiculo {
             this.tiquete = tiquete;
     }
 
+    /**
+     * Obtiene la placa del vehículo.
+     *
+     * @return placa del vehículo
+     */
     public String getPlaca() {
         return placa;
     }
 
+        /**
+     * Modifica la placa del vehículo.
+     *
+     * @param placa nueva placa
+     */
     public void setPlaca (String placa) {
         this.placa = placa;
     }
 
+        /**
+     * Obtiene la marca del vehículo.
+     *
+     * @return marca del vehículo
+     */
     public String getMarca() {
         return marca;
     }
 
+        /**
+     * Modifica la marca del vehículo.
+     *
+     * @param marca nueva marca
+     */
     public void setMarca (String marca) {
         this.marca = marca;
     }
 
+        /**
+     * Obtiene la hora de ingreso.
+     *
+     * @return hora de ingreso
+     */
     public int getHoraIngreso() {
         return horaIngreso;
     }
 
+        /**
+     * Modifica la hora de ingreso.
+     *
+     * @param horaIngreso nueva hora de ingreso
+     */
     public void setHoraIngreso (int horaIngreso) {
         this.horaIngreso = horaIngreso;
     }
 
+        /**
+     * Obtiene el tiquete asociado al vehículo.
+     *
+     * @return tiquete del vehículo
+     */
     public Tiquete tiquete() {
         return tiquete;
     }
 
+        /**
+     * Modifica el tiquete asociado al vehículo.
+     *
+     * @param tiquete nuevo tiquete
+     */
     public void setTiquete (Tiquete tiquete) {
         this.tiquete = tiquete;
     }
 
+        /**
+     * Calcula el valor que debe pagar el vehículo.
+     *
+     * @param horaSalida hora de salida entre 0 y 23
+     * @param tarifaHora tarifa cobrada por cada hora
+     * @return valor total a pagar
+     */
     public int calcularCobro(int horaSalida, int tarifaHora) {
 
         int horasTranscurridas = horaSalida - horaIngreso;
@@ -52,6 +110,11 @@ public class Vehiculo {
         return horasTranscurridas * tarifaHora;
     }
 
+        /**
+     * Muestra la información del vehículo.
+     *
+     * @return información del vehículo
+     */
     public Tiquete getTiquete() {
         return tiquete;
     }
